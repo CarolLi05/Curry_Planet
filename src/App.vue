2f70561100c544232a04fbef952a1ca1e5db0bc1
Carol Li <dinosaur0514@gmail.com>
2022-07-23 16:53:06 +0800

    載入 scss

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link :to="{name: 'Dashboard'}">admin</router-link> |
   </nav>
   <router-view/>
 </template>
 
 <style lang="scss">
+@import "./assets/stylesheet/all.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
