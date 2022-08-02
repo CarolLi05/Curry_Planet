73ebf2c47f3ddb8e0fe503119b1df143747f0199
Carol Li <dinosaur0514@gmail.com>
2022-08-02 23:20:21 +0800

    新增 meterial icon

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <router-link to="/about">About</router-link> |
     <router-link :to="{name: 'Dashboard'}">admin</router-link> |
   </nav>
+
   <router-view/>
 </template>
 
--- src/components/navbar/FrontNavbar.vue
+++ src/components/navbar/FrontNavbar.vue
@@ -1,9 +1,10 @@
 <template>
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
       <h1 class="navbar-brand">
         <router-link class="d-block logo-link" to="/">Curry Planet</router-link>
       </h1>
+      <span class="material-icons-outlined h1">shopping_cart</span>
   </div>
 </nav>
 </template>
