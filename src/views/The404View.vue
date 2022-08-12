cbf1ed4f141c25ca21da47f7e502d9d13fea0745
Carol Li <dinosaur0514@gmail.com>
2022-08-12 15:21:39 +0800

    頁面改為深色

--- src/views/The404View.vue
+++ src/views/The404View.vue
@@ -1,7 +1,7 @@
 <template>
-<div class="d-flex flex-column align-items-center w-100 vh-100 mt-10">
-  <div class="d-flex justify-content-center align-item-center mt-10 w-100">
-    <div class="w-100 mt-10">
+<div class="bg-dark text-white d-flex flex-column align-items-center w-100 vh-100 pt-10">
+  <div class="d-flex justify-content-center align-item-center pt-10 w-100">
+    <div class="w-100 pt-10">
       <div class="four-layer position-relative">
         <img src="../assets/images/4.svg" alt="" class="four-left position-absolute">
         <img src="../assets/images/4.svg" alt="" class="four-right position-absolute">
@@ -10,8 +10,8 @@
         <img src="../assets/images/404.svg" alt="" class="img-404 w-25">
       </div>
       <div class="text-center">
-        <h3 class="mt-8 text-center">哎呀，好像有東西不見了</h3>
-        <p class="text-center mt-3">目前找不到你要的頁面</p>
+        <h3 class="pt-8 text-center">哎呀，好像有東西不見了</h3>
+        <p class="text-center pt-3">目前找不到你要的頁面</p>
         <router-link to="/" class="btn btn-primary rounded-pill">返回首頁</router-link>
       </div>
     </div>
