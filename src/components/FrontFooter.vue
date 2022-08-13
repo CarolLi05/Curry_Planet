dcb362360de77f0b712a86315f75f169e2d3429d
Carol Li <dinosaur0514@gmail.com>
2022-08-13 21:10:08 +0800

    修改 footer 結構，新增社群 icon

--- src/components/FrontFooter.vue
+++ src/components/FrontFooter.vue
@@ -1,38 +1,52 @@
 <template>
   <footer class="footer bg-dark text-light">
-    <div class="container-fluid">
-      <div class="d-flex justify-content-between p-8">
-        <div class="d-flex justify-content-center align-item-center">
-          <img src="../assets/images/Logo-circle.svg" alt="" class="logo w-50">
+    <div class="container">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 p-8">
+        <div class="col">
+          <div class="">
+            <img src="../assets/images/Logo-circle.svg" alt="" class="logo w-50">
+          </div>
         </div>
-        <div class="d-flex justify-content-between">
-          <ul class="list-unstyled">
-            <li class="mb-2">
-              <router-link to="/about">關於我們</router-link>
+        <div class="col">
+          <ul class="nav flex-column list-unstyled">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about">關於我們</router-link>
             </li>
-            <li class="mb-2">
-              <router-link to="/products">菜單底加</router-link>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/products">菜單底加</router-link>
             </li>
-            <li class="mb-2">
-              <router-link to="/booking">預訂位子</router-link>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/booking">預訂位子</router-link>
             </li>
-            <li class="mb-2">
-              <router-link to="/contact">聯絡我們</router-link>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/contact">聯絡我們</router-link>
             </li>
           </ul>
-          <div class="footer-right">
-            <div class="d-flex justify-content-between">
-              <div class="footer-info">
-                <p class="mb-2"></p>
-                <p class="mb-2">電話：02-123-4567</p>
-                <p class="mb-2">curry_planet@gmail.com</p>
-                <p class="mb-2">MON-SUN  14:00-20:00</p>
-              </div>
-              <ul class="d-flex align-items-center">
-                <span class="material-icon"></span>
-              </ul>
-            </div>
-          </div>
+        </div>
+        <div class="col">
+          <ul class="nav flex-column list-unstyled">
+            <li class="nav-item py-1">台南市東區長榮路一段234巷17號</li>
+            <li class="nav-item py-1">電話：02-123-4567</li>
+            <li class="nav-item py-1">curry_planet@gmail.com</li>
+            <li class="nav-item py-1">MON-SUN  14:00-20:00</li>
+          </ul>
+          <ul class="nav d-flex align-items-center list-unstyled">
+            <li class="nav-item">
+              <router-link class="nav-link ps-0 me-4" to="/">
+                <i class="h4 fa-brands fa-square-facebook"></i>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link ps-0 me-4" to="/">
+                <i class="h4 fa-brands fa-instagram"></i>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link ps-0 me-4" to="/">
+                <i class="fa-brands fa-line h4"></i>
+              </router-link>
+            </li>
+          </ul>
         </div>
       </div>
       <p class="text-center text-xs py-5">Copyright ©Curry Planet All Rights Reserved. Designed by Carol Li</p>
