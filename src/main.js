3197551e2e65c7d06eaedf0b6cc624a6904624c3
Carol Li <dinosaur0514@gmail.com>
2022-08-13 21:10:31 +0800

    新增 font-awesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
+
+// axios
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+
+// fontawsome
+import '@fortawesome/fontawesome-free'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
 import App from './App.vue'
 import router from './router'
 
