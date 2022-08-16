ab6896477d26d6278a715d20d5e9eac9b60b6db1
Carol Li <dinosaur0514@gmail.com>
2022-08-16 17:50:45 +0800

    新增各頁面 title、連結狀態顯示、後台重新導向

--- src/router/index.js
+++ src/router/index.js
@@ -17,32 +17,50 @@ const routes = [
       {
         path: '',
         name: 'home',
-        component: () => import('../views/HomeView.vue')
+        component: () => import('../views/HomeView.vue'),
+        meta: {
+          title: 'Curry Planet'
+        }
       },
       {
         path: 'products',
         name: 'products',
-        component: () => import('../views/front/ProductsView.vue')
+        component: () => import('../views/front/ProductsView.vue'),
+        meta: {
+          title: '商品列表｜Curry Planet'
+        }
       },
       {
         path: 'product/:id',
         name: 'product',
-        component: () => import('../views/front/ProductView.vue')
+        component: () => import('../views/front/ProductView.vue'),
+        meta: {
+          title: '商品細節｜Curry Planet'
+        }
       },
       {
         path: 'booking',
         name: 'booking',
-        component: () => import('../views/front/BookingView.vue')
-      },
-      {
-        path: 'confirm',
-        name: 'orderConfirm',
-        component: () => import('../views/front/OrderConfirm.vue')
+        component: () => import('../views/front/BookingView.vue'),
+        meta: {
+          title: '訂位頁面｜Curry Planet'
+        }
       },
       {
         path: 'create',
         name: 'orderCreate',
-        component: () => import('../views/front/OrderCreate.vue')
+        component: () => import('../views/front/OrderCreate.vue'),
+        meta: {
+          title: '建立訂單｜Curry Planet'
+        }
+      },
+      {
+        path: 'confirm',
+        name: 'orderConfirm',
+        component: () => import('../views/front/OrderConfirm.vue'),
+        meta: {
+          title: '完成訂單｜Curry Planet'
+        }
       },
       {
         path: 'coupon',
@@ -54,23 +72,49 @@ const routes = [
   {
     path: '/admin',
     name: 'Dashboard',
-    component: () => import('../views/dashboard/DashboardView.vue')
+    component: () => import('../views/admin/DashboardView.vue'),
+    redirect: '/admin/products',
+    children: [
+      {
+        path: 'products',
+        name: 'adminProducts',
+        component: () => import('../views/admin/ProductsView.vue'),
+        meta: {
+          title: '後台商品管理｜Curry Planet'
+        }
+      },
+      {
+        path: 'booking',
+        name: 'adminBooking',
+        component: () => import('../views/admin/BookingView.vue'),
+        meta: {
+          title: '後台訂位管理｜Curry Planet'
+        }
+      }
+    ]
   },
   {
     path: '/login',
     name: 'login',
-    component: () => import('../views/LoginView.vue')
+    component: () => import('../views/LoginView.vue'),
+    meta: {
+      title: '後台登入｜Curry Planet'
+    }
   },
   {
     path: '/:pathMatch(.*)*',
     name: '404',
-    component: () => import('../views/The404View.vue')
+    component: () => import('../views/The404View.vue'),
+    meta: {
+      title: '404 Not Found｜Curry Planet'
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
+  linkActiveClass: 'active'
 })
 
 export default router
