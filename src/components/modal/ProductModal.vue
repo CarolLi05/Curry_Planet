d0e0b0208472486685e358dce189f67787502702
Carol Li <dinosaur0514@gmail.com>
2022-09-01 20:36:29 +0800

    新增 modal

---
+++ src/components/modal/ProductModal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="modal fade" ref="modal" tabindex="-1" aria-labelledby="productModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>Modal body text goes here.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BsModal from 'bootstrap/js/dist/modal'
+
+export default {
+  data () {
+    return {
+      modal: ''
+    }
+  },
+  methods: {
+    openModal () {
+      this.modal.show()
+    },
+    closeModal () {
+      this.modal.hide()
+    },
+    updateProduct () {
+      this.$http.post(`${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/product`)
+        .then((res) => {
+          console.log(res)
+        })
+        .catch((err) => {
+          console.dir(err)
+        })
+    }
+  },
+  mounted () {
+    this.modal = new BsModal(this.$refs.modal)
+  }
+}
+</script>
--- src/views/admin/ProductsView.vue
+++ src/views/admin/ProductsView.vue
@@ -1,40 +1,71 @@
 <template>
-<h2>後台商品管理</h2>
-<button class="btn btn-outline-primary" id="getProducts">取得產品列表</button>
-<button class="btn btn-outline-primary" id="addProduct">新增一個產品</button>
-<button class="btn btn-outline-primary" id="delProduct">刪除一個產品</button>
-<br>
-{{productsAll}}
-<div class="row row-cols-1 row-cols-md-3 g-3">
-  <div class="col" v-for="product in productsAll" :key="product.id">
-    <div class="card board-0">
-      <img :src="product.imageUrl" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">{{product.title}}</h5>
-        <p class="card-text">{{product.description}}</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card board-0">
-      <img src="" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text mb-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
+<div class="d-flex my-3">
+  <button class="btn btn-outline-primary" id="getProducts">取得產品列表</button>
+  <button class="btn btn-outline-primary" id="addProduct">新增一個產品</button>
+  <button class="btn btn-outline-primary" id="delProduct">刪除一個產品</button>
 </div>
+{{productsAll}}
+<div class="d-flex justify-content-between align-items-center mb-4">
+  <div class="header d-flex align-items-center">
+    <h2 class="pe-3">後台商品管理</h2>
+    <select name="" id="" class="form-select w-auto">
+      <option value="">全部</option>
+    </select>
+  </div>
+  <button type="button" class="btn btn-primary">
+    <i class="fa-solid fa-plus"></i>
+    新增商品
+  </button>
+</div>
+<table class="table table-hover align-middle">
+  <thead>
+    <tr>
+      <th scope="col" class="fw-bold">類別</th>
+      <th scope="col">名稱</th>
+      <th scope="col">原價</th>
+      <th scope="col">售價</th>
+      <th scope="col">是否上架</th>
+      <th scope="col">編輯｜刪除</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="product in productsAll" :key="product.id">
+      <th scope="row">{{ product.category }}</th>
+      <td>{{product.title}}</td>
+      <td>NT$ {{ product.origin_price }}</td>
+      <td>NT$ {{ product.price }}</td>
+      <td>{{product.is_enabled}}</td>
+      <td>
+        <div class="d-flex align-items-center">
+          <button type="button" class="btn btn-sm btn-outline-primary rounded me-1 editBtn" @click="openProductModal">
+            <!-- ('edit', item) -->
+            <i class="fa-solid fa-pencil"></i>
+          </button>｜
+          <button type="button" class="btn btn-sm btn-outline-danger rounded ms-1 delBtn">
+            <!-- @click="openModal ('delete', item)" -->
+            <i class="fa-solid fa-trash"></i>
+          </button>
+        </div>
+      </td>
+    </tr>
+  </tbody>
+</table>
+<ProductModal ref="ProductModal"></ProductModal>
 </template>
 
 <script>
+// import { ref } from 'vue'
+import ProductModal from '@/components/modal/ProductModal'
 export default {
   data () {
     return {
-      productsAll: []
+      productsAll: [],
+      // count: ref(10),
+      tempProduct: {} // 新增的資料存在這裡
     }
+  },
+  components: {
+    ProductModal
   },
   methods: {
     getAllProducts () {
@@ -46,6 +77,9 @@ export default {
         .catch((err) => {
           console.dir(err)
         })
+    },
+    openProductModal () {
+      this.$refs.modal.openModal()
     }
   },
   mounted () {
