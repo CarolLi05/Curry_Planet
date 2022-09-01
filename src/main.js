ab1816e294d19d2c1133dfbd1d2d2b3043d94e5d
Carol Li <dinosaur0514@gmail.com>
2022-09-01 20:37:13 +0800

    新增 pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 
+// Pinia
+import { createPinia } from 'pinia'
+
 // axios
 import axios from 'axios'
 import VueAxios from 'vue-axios'
@@ -12,8 +15,11 @@ import '@fortawesome/fontawesome-free/js/all.js'
 import App from './App.vue'
 import router from './router'
 
+// Pinia
+const pinia = createPinia()
 const app = createApp(App)
 app.use(VueAxios, axios)
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
